31d68f9309e0d4b6f5ed381f09fd4f6a073d50d6
dhabensky <dhabensky@yandex.ru>
2019-03-16 03:15:30 +0300

    initial commit

---
+++ lib/src/androidTest/java/com/dhabensky/svm/ExampleInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.dhabensky.svm
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("com.dhabensky.svm", appContext.packageName)
+    }
+}
